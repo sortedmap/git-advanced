b7df8316f1d68a1be2557d4a3063ea55413c6c95
Daniil Pilipenko <sortedmap@gmail.com>
2022-08-17 14:02:40 +0300

    comment added

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
     document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('button').addEventListener('click', function(){
             let text = document.getElementById('text').value;
-            alert('Длина строки: ' + text)
+            alert('Длина строки: ' + text);
+            //some text
         });
     });
 })();
