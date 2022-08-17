6517aa8baad2d0adcdf33ef71fb8a55bf311d4ac
Daniil Pilipenko <sortedmap@gmail.com>
2022-08-17 12:51:33 +0300

    removed alert

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
     document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('button').addEventListener('click', function(){
             let text = document.getElementById('text').value;
-            alert('Длина текста: ' + text.length);
         });
     });
 })();
