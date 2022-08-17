fb8570d9a4a1c9cc84aeeeed786ea79aa7f53be8
Daniil Pilipenko <sortedmap@gmail.com>
2022-08-17 11:56:45 +0300

    files added

---
+++ script.js
@@ -0,0 +1,8 @@
+(function(){
+    document.addEventListener('DOMContentLoaded', function(){
+        document.getElementById('button').addEventListener('click', function(){
+            let text = document.getElementById('text').value;
+            alert('Длина текста: ' + text.length);
+        });
+    });
+})();
