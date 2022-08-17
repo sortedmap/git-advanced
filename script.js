cca3960d1b9a92adb8f7cfe0f66b638c48ff7e59
Daniil Pilipenko <sortedmap@gmail.com>
2022-08-17 13:49:54 +0300

    alert added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('button').addEventListener('click', function(){
             let text = document.getElementById('text').value;
+            alert('Длина строки: ' + text)
         });
     });
 })();
