8c8a2e841b7ad01df366597c02990a24eae9193a
Daniil Pilipenko <sortedmap@gmail.com>
2022-08-19 12:05:05 +0300

    test changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
         document.getElementById('button').addEventListener('click', function(){
             let text = document.getElementById('text').value;
             alert('Длина текста: ' + text.length);
+            // some comment
         });
     });
 })();
